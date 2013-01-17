2eef33a4d922905bdd7601a740d709232c5bc801
jbspeakr <jbspeakr@funkblocka.de>
2013-01-17 15:30:44 +0100

    added required package files

--- dmakr/urls.py
+++ dmakr/urls.py
@@ -1,13 +1,8 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import patterns, url
 
 from dmakr import views
 
 urlpatterns = patterns('dmakr.urls',
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, {}, name='index'),
     url(r'^domainhack/(?P<word>([a-z]+)/$)', views.hack, {}, name='domainhack'),
 )
