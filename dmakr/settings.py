2f42bd78b598ec3e2cfdb0e3813a70a5e023b5ea
jbspeakr <jbspeakr@funkblocka.de>
2013-05-16 13:52:32 +0200

    cleanuo por new version

--- dmakr/settings.py
+++ dmakr/settings.py
@@ -138,7 +138,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'dmakr',
-    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
