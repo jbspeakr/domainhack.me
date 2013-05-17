4f761d4ab8b7aa1ddaea0074a90cb0924e3f2e37
jbspeakr <jbspeakr@funkblocka.de>
2013-05-17 20:09:48 +0200

    transformed project from python/djnago to plain AngularJS, JS and JSON

--- dmakr/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from dmakr import views
-
-urlpatterns = patterns('dmakr.urls',
-    url(r'^$', views.index, {}, name='index'),
-    url(r'^domainhack/(?P<word>([a-z]+)/$)', views.hack, {}, name='domainhack'),
-)
