83c6c12cdeb7a95d802bafff2e00faa952b4ec79
Jan Brennenstuhl <jan.brennenstuhl@immobilienscout24.de>
2016-06-16 14:33:50 +0200

    try fixing deployment

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -10,6 +10,7 @@ echo "Starting to deploy..."
  cp -r ../dist/* ./dist/
  cp ../index.html ./index.html
  cp ../CNAME ./CNAME
+ git add .
  git commit -a -m "Deployed to Github Pages"
  git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
